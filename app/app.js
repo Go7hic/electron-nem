c52e222e2a007f237fbf836bf1f55bef72e4824c
Go7hic <gtfx0209@qq.com>
2016-02-29 23:00:42 +0800

    add osx version

--- app/app.js
+++ app/app.js
@@ -1,24 +1,49 @@
-// Here is the starting point for your application code.
-// All stuff below is just to show you how it works. You can delete all of it.
+var remote = require('remote');
+const ipc = require('electron').ipcRenderer;
+var browserWindow = remote.getCurrentWindow();
+var webView;
+var isMini = false;
 
-// Use new ES6 modules syntax for everything.
-import os from 'os'; // native node.js module
-import { remote } from 'electron'; // native electron module
-import jetpack from 'fs-jetpack'; // module loaded from npm
-import { greet } from './hello_world/hello_world'; // code authored by you in this project
-import env from './env';
+var playControl = {};
 
-console.log('Loaded environment variables:', env);
-
-var app = remote.app;
-var appDir = jetpack.cwd(app.getAppPath());
-
-// Holy crap! This is browser window with HTML and stuff, but I can read
-// here files like it is node.js! Welcome to Electron world :)
-console.log('The author of this app is:', appDir.read('package.json', 'json').author);
-
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('greet').innerHTML = greet();
-    document.getElementById('platform-info').innerHTML = os.platform();
-    document.getElementById('env-name').innerHTML = env.name;
+ipc.on('playControl', (e, msg)=>
+{
+    webview.send('playControl', msg);
 });
+
+ipc.on('windowControl', (e, msg)=>
+{
+  if(msg == 'showMain')
+  {
+    browserWindow.show();
+    if(isMini)
+    {
+      toggleMiniMode();
+    }
+  }
+});
+
+function toggleMiniMode()
+{
+  if(isMini)
+  {
+    browserWindow.setSize(982, 600);
+    browserWindow.setAlwaysOnTop(false);
+browserWindow.setMinimumSize(982, 600);
+  }else
+  {
+
+    browserWindow.setSize(350, 100);
+browserWindow.setMaximumSize(350, 100);
+    browserWindow.setAlwaysOnTop(true);
+  }
+
+document.getElementById("title-text").style.width = isMini ? "900px" : "278px";
+
+  document.getElementById("btn-minimize").style.display =  isMini ?  "inherit" : "none";
+  webview.send('windowControl', 'toggleMiniMode');
+
+
+  isMini = !isMini;
+
+}
