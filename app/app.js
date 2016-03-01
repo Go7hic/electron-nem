894c452ebf5764aeb01845530951232e2564e8f7
Go7hic <gtfx0209@qq.com>
2016-03-01 11:24:12 +0800

    code style

--- app/app.js
+++ app/app.js
@@ -6,44 +6,37 @@ var isMini = false;
 
 var playControl = {};
 
-ipc.on('playControl', (e, msg)=>
-{
+ipc.on('playControl', (e, msg) => {
     webview.send('playControl', msg);
 });
 
-ipc.on('windowControl', (e, msg)=>
-{
-  if(msg == 'showMain')
-  {
-    browserWindow.show();
-    if(isMini)
-    {
-      toggleMiniMode();
+ipc.on('windowControl', (e, msg) => {
+    if (msg == 'showMain') {
+        browserWindow.show();
+        if (isMini) {
+            toggleMiniMode();
+        }
     }
-  }
 });
 
-function toggleMiniMode()
-{
-  if(isMini)
-  {
-    browserWindow.setSize(982, 600);
-    browserWindow.setAlwaysOnTop(false);
-browserWindow.setMinimumSize(982, 600);
-  }else
-  {
+function toggleMiniMode() {
+    if (isMini) {
+        browserWindow.setSize(982, 600);
+        browserWindow.setAlwaysOnTop(false);
+        browserWindow.setMinimumSize(982, 600);
+    } else {
 
-    browserWindow.setSize(350, 100);
-browserWindow.setMaximumSize(350, 100);
-    browserWindow.setAlwaysOnTop(true);
-  }
+        browserWindow.setSize(350, 100);
+        browserWindow.setMaximumSize(350, 100);
+        browserWindow.setAlwaysOnTop(true);
+    }
 
-document.getElementById("title-text").style.width = isMini ? "900px" : "278px";
+    document.getElementById("title-text").style.width = isMini ? "900px" : "278px";
 
-  document.getElementById("btn-minimize").style.display =  isMini ?  "inherit" : "none";
-  webview.send('windowControl', 'toggleMiniMode');
+    document.getElementById("btn-minimize").style.display = isMini ? "inherit" : "none";
+    webview.send('windowControl', 'toggleMiniMode');
 
 
-  isMini = !isMini;
+    isMini = !isMini;
 
 }
